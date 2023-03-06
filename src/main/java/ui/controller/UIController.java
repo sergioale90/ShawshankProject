55016158ed3c4b53082aad014470d0323958ef95
Maria Hurtado <Maria.Hurtado@jala.university>
2023-03-05 22:16:57 -0400

    Adding the update from UI Controller

--- src/main/java/ui/controller/UIController.java
+++ src/main/java/ui/controller/UIController.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2023 Jala University.
- *
+ * <p>
  * This software is the confidential and proprieraty information of Jala University
  * ("Confidential Information"). You shall not disclose such Confidential
  * Information and shall use it only in accordance with the terms of the
@@ -40,6 +40,7 @@ public class UIController {
     public void setContent(String content) {
         this.content = content;
     }
+
     public String getName() {
         return name;
     }
@@ -47,6 +48,7 @@ public class UIController {
     public void setName(String name) {
         this.name = name;
     }
+
     public String getSlug() {
         return slug;
     }
@@ -54,6 +56,7 @@ public class UIController {
     public void setSlug(String slug) {
         this.slug = slug;
     }
+
     public String getDescription() {
         return description;
     }
@@ -61,6 +64,7 @@ public class UIController {
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getMessage() {
         return message;
     }
